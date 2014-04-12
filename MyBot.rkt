b7f7331d6384464bd7db264b9b201ea5076d3b67
Quon Lu <ixitle@gmail.com>
2014-04-12 13:28:02 +0800

    add log commit d4a979b1 msg wrong, correct: bugfix: key value cons in wrong order, use hash->list instead

--- MyBot.rkt
+++ MyBot.rkt
@@ -2,11 +2,19 @@
 
 (require "ants.rkt")
 
+(define out (open-output-file "game_logs/out.log" #:exists 'replace))
+
+(define (log str)
+  (write str out))
+
 (define DIRECTIONS '(N E S W))
 
-(define (do-setup) (void))
+(define (do-setup map-data) (void));;(log (format "~a\n" map-data)))
 
-(define (do-turn)
+;;(define (move-ant a directions targets)
+;;  (void))
+(define (do-turn map-data)
+  ;;(print-map)
   (define (move-ant a)
     (define (try-direction dir)
       (and (passable? (destination a dir))
@@ -19,16 +27,17 @@
     (match (read)
      ['ready
       (setup map-data)
-      (do-setup)
+      (do-setup map-data)
       (finish-turn)
       (loop '())]
      ['go
       (update map-data)
-      (do-turn)
+      (do-turn map-data)
       (finish-turn)
       (loop '())]
      [(? eof-object?) (void)]
      [x (loop (cons x map-data))]
-     )))
+     ))
+  (close-output-port out))
 
 (main)
